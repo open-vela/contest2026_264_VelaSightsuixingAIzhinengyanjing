f2e81e08f35e75c341dd1a829cf739a3fca36d13
“zhaoyutao1” <zhaoyutao1@xiaomi.com>
2026-07-31 14:36:48 +0800

    chore: remove PSRAM test bringup hook

--- board/beken/boards/bk7258/bk7258-ap/src/bk7258_bringup.c
+++ board/beken/boards/bk7258/bk7258-ap/src/bk7258_bringup.c
@@ -9,8 +9,6 @@
 
 #include <arch/board/board.h>
 
-#include "bk7258_psram_test.h"
-
 int bk7258_pwc_start(void);
 int bk7258_motor_setup(void);
 int bk7258_power_key_motor_start(void);
@@ -33,14 +31,6 @@ int bk7258_bringup(void)
     {
       return ret;
     }
-
-#ifdef CONFIG_BK7258_PSRAM_TEST
-  ret = bk7258_psram_test(0, NULL);
-  if (ret < 0)
-    {
-      return ret;
-    }
-#endif
 
   ret = bk7258_power_key_motor_start();
   if (ret < 0)
